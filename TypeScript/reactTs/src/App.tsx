5b48fa49feee036bc7921b15a721a79392aeafc1
Zubair <zubairsanaullah1122@gmail.com>
2026-04-21 14:10:39 +0500

    Counter.tsx added

--- TypeScript/reactTs/src/App.tsx
+++ TypeScript/reactTs/src/App.tsx
@@ -1,5 +1,16 @@
-import { ChaiCard } from './components/ChaiCard.tsx'
 import './App.css'
+import { ChaiCard } from './components/ChaiCard.tsx'
+import { Counter } from './components/Counter.tsx'
+import { ChaiList } from './components/ChaiList.tsx'
+
+import type { Chai } from './types/types.ts'
+
+const chaiList: Chai[] = [
+  {id: 1, name: "Masala Chai", price: 70, isSpecial: true},
+  {id: 2, name: "Ginger Chai", price: 60, isSpecial: false},
+  {id: 3, name: "Cardamom Chai", price: 65, isSpecial: true},
+  {id: 4, name: "Lemon Tea", price: 50, isSpecial: false},
+]
 
 function App() {
 
@@ -8,6 +19,12 @@ function App() {
     <div>
       <h1>Hello World</h1>
       <ChaiCard name="Masala Chai" price={70} isSpecial={true} />
+     </div>
+     <div>
+      <Counter />
+     </div>
+     <div>
+      <ChaiList items={chaiList} />
      </div>
     </>
   )
---
+++ TypeScript/reactTs/src/components/ChaiList.tsx
@@ -0,0 +1,23 @@
+import type { Chai } from "../types/types"
+import { ChaiCard } from "./ChaiCard"
+
+interface ChaiListProps{
+    items: Chai[]
+}
+
+export function ChaiList({items}: ChaiListProps) {
+    return (
+        <div>
+            {items.map((chai) => (
+                <ChaiCard 
+                    key={chai.id} 
+                    name={chai.name} 
+                    price={chai.price} 
+                    isSpecial={chai.isSpecial} 
+                />
+            ))}
+        </div>
+    )
+}
+
+export default ChaiList
