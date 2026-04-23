c22cd9807d165cef7a085d4dbade41c302ab8cc0
Zubair <zubairsanaullah1122@gmail.com>
2026-04-23 11:45:44 +0500

    installed npm i dotenv and converted into env file

--- img-project/src/services/storage.service.js
+++ img-project/src/services/storage.service.js
@@ -1,19 +1,18 @@
-const ImageKit = require('@imagekit/nodejs');
+const ImageKit = require("@imagekit/nodejs");
 
 const imagekit = new ImageKit({
-    privateKey: 'private_XT+SAz5J8vVCBrzOEpqSq4A9AJ8=',
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
 });
 
 async function uploadFile(buffer) {
+  console.log(buffer);
 
-    console.log(buffer)
+  const result = await imagekit.files.upload({
+    file: buffer.toString("base64"),
+    fileName: "image.jpg",
+  });
 
-    const result = await imagekit.files.upload({
-        file: buffer.toString('base64'),
-        fileName: 'image.jpg'
-    })
-
-    return result;
+  return result;
 }
 
 module.exports = uploadFile;
