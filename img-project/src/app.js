9084fb0c30416a0327ac087ff51ffe4ccd448220
Zubair <zubairsanaullah1122@gmail.com>
2026-04-23 11:57:49 +0500

    Post API created

--- img-project/src/app.js
+++ img-project/src/app.js
@@ -1,21 +1,27 @@
-const express = require('express');
-const multer = require('multer');
-const uploadFile = require('./services/storage.service');
+const express = require("express");
+const multer = require("multer");
+const uploadFile = require("./services/storage.service");
+const postModel = require("./models/post.model");
 
 const app = express();
 app.use(express.json());
 
 const upload = multer({
-    storage: multer.memoryStorage()
-})
+  storage: multer.memoryStorage(),
+});
 
-app.post('/create-post', upload.single('image'), async (req, res) => {
-    console.log(req.body)
-    console.log(req.file)
+app.post("/create-post", upload.single("image"), async (req, res) => {
+  const result = await uploadFile(req.file.buffer);
 
-    const result = await uploadFile(req.file.buffer)
+  const post = await postModel.create({
+    image: result.url,
+    caption: req.body.caption,
+  });
 
-    console.log(result);
-})
+  return res.status(201).json({
+    message: "post created successfully",
+    post,
+  });
+});
 
 module.exports = app;
