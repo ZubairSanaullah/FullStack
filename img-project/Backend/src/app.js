232f22faa89a7c7b1c62c8c649e7032932dcf548
Zubair <zubairsanaullah1122@gmail.com>
2026-04-23 12:09:56 +0500

    moved every folder to backend

--- img-project/Backend/src/app.js
+++ img-project/Backend/src/app.js
@@ -24,4 +24,13 @@ app.post("/create-post", upload.single("image"), async (req, res) => {
   });
 });
 
+app.get("/posts", async (req, res) => {
+  const posts = await postModel.find();
+
+  return res.status(200).json({
+    message: "posts fetched successfully",
+    posts,
+  });
+});
+
 module.exports = app;
